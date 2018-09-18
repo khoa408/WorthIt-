dc96c4ca1c744e6b94f2f65a6b15545417b1a37a
Khoa Tran <khoa.d.tran97@gmail.com>
2018-09-17 23:26:34 -0700

    Initial Commit

---
+++ WorthIt?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorthIt?
+//
+//  Created by Khoa Tran on 9/17/18.
+//  Copyright © 2018 Khoa Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
